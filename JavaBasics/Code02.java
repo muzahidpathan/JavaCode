4dec8efd1849e9f9f2a412dca1c68deaf2fd4ba2
Muzahid Pathan <112048518+muzahidpathan@users.noreply.github.com>
2025-05-05 19:11:58 +0000

    Sum of Two number

---
+++ JavaBasics/Code02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Code02
+{
+    public static void main(String rgs[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter any two numbers");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=a+b;
+        System.out.print(c);
+    }
+}
